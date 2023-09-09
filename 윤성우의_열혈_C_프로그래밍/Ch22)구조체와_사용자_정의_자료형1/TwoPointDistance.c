b20bba93d3223254bf67990d0fad9e756503f294
seung-ho back <ilikeapink@naver.com>
2023-09-09 07:12:33 +0000

    (미완)구조체 두 점 사이 거리 구하기

--- 윤성우의_열혈_C_프로그래밍/Ch22)구조체와_사용자_정의_자료형1/TwoPointDistance.c
+++ 윤성우의_열혈_C_프로그래밍/Ch22)구조체와_사용자_정의_자료형1/TwoPointDistance.c
@@ -19,7 +19,6 @@ int main(){
 
     /* 두 점간의 거리 계산 공식 */
     distance = sqrt((double)((pos1.xpos - pos2.xpos) * (pos1.xpos - pos2.xpos) + (pos1.ypos - pos2.ypos) * (pos1.ypos - pos2.ypos)));
-
     printf("두 점의 거리는 %g 입니다.\n", distance);
 
     return 0;
