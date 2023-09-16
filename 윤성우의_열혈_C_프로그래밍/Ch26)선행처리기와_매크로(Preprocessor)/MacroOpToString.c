2689d14bfea463c2374c9afa3f3a7c796558c465
seung-ho back <ilikeapink@naver.com>
2023-09-16 08:56:11 +0000

    .

--- 윤성우의_열혈_C_프로그래밍/Ch26)선행처리기와_매크로(Preprocessor)/MacroOpToString.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#define STRING_JOB(A, B)    #A "의 직업은 " #B "입니다."
-
-int main(){
-    printf("%s \n", STRING_JOB(이동춘, 나무꾼));
-    printf("%s \n", STRING_JOB(한상순, 사냥꾼));
-
-    return 0;
-}
